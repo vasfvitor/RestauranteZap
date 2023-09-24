00a97d489ad9e02862fa45e068534a93e9d60c9f
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-09-23 21:05:46 -0300

    export DADOS

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,19 @@
 
 export const SITE_TITLE = 'Menu Online';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const DADOS = {
+        nome: "Restaurante Exemplo",
+        endereco: {
+          rua: "Rua do Restaurante, 123",
+          cidade: "Cidade Exemplo",
+          estado: "Estado Exemplo",
+          numero: "12345",
+        },
+        telefone: "(21) 123 456 789",
+        social: {
+          instagram: "https://www.instagram.com/perfil",
+          facebook: "https://www.facebook.com/perfil/",
+          whatsapp: "https://wa.me/",
+        },
+      };
+     
