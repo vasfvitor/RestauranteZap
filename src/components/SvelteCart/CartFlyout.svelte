85c0fc2c43f6499de5ef8e4bbcf6e4e9da265f2a
vasfvitor <61759797+vasfvitor@users.noreply.github.com>
2023-10-05 16:28:35 -0300

    cart preview modal
    items on cart counter

--- src/components/SvelteCart/CartFlyout.svelte
+++ src/components/SvelteCart/CartFlyout.svelte
@@ -1,46 +1,43 @@
 <script>
-    import { isCartOpen, cartItems, cartSubTotal } from '~/components/SvelteCart/cartStore';
+    import { cartItems } from '~/components/SvelteCart/cartStore';
     import AddToCartForm from '~/components/SvelteCart/AddToCartForm.svelte';
-    import CartTotal from '~/components/Cart/CartTotal.svelte';
     import { fade } from 'svelte/transition';
     // import * as eases from 'svelte/easing';
 </script>
 
-{#if $isCartOpen}
-    {#if Object.values($cartItems).length}
-        <aside class="mx-auto w-full">
-            {#each Object.values($cartItems) as cartItem}
-                {#if cartItem.quantity > 0}
-                    <div
-                        in:fade={{ duration: 400 }}
-                        out:fade={{ duration: 250 }}
-                        class=" my-4 flex w-full items-center text-start"
-                    >
-                        <img class="mask mask-squircle mx-4 h-12 w-12" src="/temp.png" alt={cartItem.name} />
+{#if Object.values($cartItems).length}
+    <aside class="mx-auto w-full">
+        {#each Object.values($cartItems) as cartItem}
+            {#if cartItem.quantity > 0}
+                <div
+                    in:fade={{ duration: 400 }}
+                    out:fade={{ duration: 250 }}
+                    class=" my-4 flex w-full items-center text-start"
+                >
+                    <img class="mask mask-squircle mx-4 h-12 w-12" src="/temp.png" alt={cartItem.name} />
 
-                        <div class=" grow text-start font-black">
-                            <p class="text-lg">{cartItem.name}</p>
-                            <p class="text-sm font-normal text-yellow-500">
-                                R${cartItem.price} | R${cartItem.totalPrice.toFixed(2)}
-                            </p>
-                        </div>
-                        <AddToCartForm itemPrice={cartItem.price} itemId={cartItem.id} itemName={cartItem.name}>
-                            <div class="join bg-accent">
-                                <button class="btn btn-square btn-accent join-item btn-sm" type="submit" value="add"
-                                    >+</button
-                                >
-                                <p class=" join-item my-auto w-4 text-center">{cartItem.quantity}</p>
-                                <button class="btn btn-square btn-accent join-item btn-sm" type="submit" value="remove"
-                                    >-</button
-                                >
-                            </div>
-                            <button value="clear" class="btn btn-error btn-sm">X</button>
-                        </AddToCartForm>
+                    <div class=" grow text-start font-black">
+                        <p class="text-lg">{cartItem.name}</p>
+                        <p class="text-sm font-normal text-yellow-500">
+                            R${cartItem.price} | R${cartItem.totalPrice.toFixed(2)}
+                        </p>
                     </div>
-                {/if}
-            {/each}
-        </aside>
-    {:else}
-        <p>Seu carrinho esta vazio :/</p>
-    {/if}
+                    <AddToCartForm itemPrice={cartItem.price} itemId={cartItem.id} itemName={cartItem.name}>
+                        <div class="join bg-accent">
+                            <button class="btn btn-square btn-accent join-item btn-sm" type="submit" value="add"
+                                >+</button
+                            >
+                            <p class=" join-item my-auto w-4 text-center">{cartItem.quantity}</p>
+                            <button class="btn btn-square btn-accent join-item btn-sm" type="submit" value="remove"
+                                >-</button
+                            >
+                        </div>
+                        <button value="clear" class="btn btn-error btn-sm">X</button>
+                    </AddToCartForm>
+                </div>
+            {/if}
+        {/each}
+    </aside>
+{:else}
+    <p>Seu carrinho esta vazio :/</p>
 {/if}
