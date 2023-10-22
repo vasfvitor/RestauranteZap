3332b9be0d485fa2919d049c07a27b2ed5ca0e03
vasfvitor <gitkey@virtuaires.com.br>
2023-10-22 00:00:43 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    daisyui: {
-        themes: [
-            {
-                lemonade: {
-                    ...require('daisyui/src/theming/themes')['[data-theme=lemonade]'],
-
-                    '.btn': {
-                        'font-weight': 'bold',
-                        'text-transform': 'none',
-                    },
-                },
-            },
-        ],
-    },
-    theme: {
-        container: {
-            center: true,
-            screens: {
-                '2xl': '1400px',
-            },
-        },
-    },
-    plugins: [require('@tailwindcss/typography'), require('daisyui')],
+   
+  
+    plugins: [require('@tailwindcss/typography')],
 };
